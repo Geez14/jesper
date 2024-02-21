5a27af51385d3c2d002f19fc8d5ab7ade0708454
Akash Lohar <94453657+akash1047@users.noreply.github.com>
2024-02-22 00:25:32 +0530

    Scanner::scan() partially implemented
    * Scanner::scan_number() contains todo!
    
    * don't touch token.rs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ mod scanner;
 fn main() {
     avideb::visible();
 
-    let mut l = scanner::Scanner::new("main".into(), "repl".into());
+    let mut l = scanner::Scanner::new("main".into(), "repl".into(), |pos, msg| {
+        println!("Lexing Error: {msg}. line {}, column {}, {}", pos.line, pos.column, pos.filename);
+    });
 
     println!("tok = {:?}", l.scan());
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,7 +1,7 @@
 use crate::token::{self, Literal, Position, Token};
 
 #[derive(Debug, Default)]
-pub struct Scanner {
+pub struct Scanner<ErrorHandler: Fn(Position, String)> {
     src: String,
     filename: String,
 
@@ -11,15 +11,23 @@ pub struct Scanner {
 
     line_offset: usize,
     line_no: usize,
+
+    err: ErrorHandler,
 }
 
-impl Scanner {
-    pub fn new(src: String, filename: String) -> Self {
+impl<E: Fn(Position, String)> Scanner<E> {
+    pub fn new(src: String, filename: String, err: E) -> Self {
         let mut l = Self {
             src,
             filename,
+            ch: b' ',
+            offset: 0,
+            rd_offset: 0,
+
+            line_offset: 0,
             line_no: 1,
-            ..Self::default()
+
+            err,
         };
         l.next();
         l
@@ -64,6 +72,19 @@ impl Scanner {
         lit
     }
 
+    fn scan_number(&mut self) -> (Token, Literal) {
+        if self.ch == b'0' {
+            match self.peek() {
+                b'b' => {}
+                b'x' | b'X' => {}
+                b'0'..=b'7' => {}
+                _ => {}
+            }
+        }
+
+        todo!()
+    }
+
     fn position(&self) -> Position {
         Position {
             filename: self.filename.clone(),
@@ -73,18 +94,114 @@ impl Scanner {
         }
     }
 
-    pub fn scan(&mut self) -> (Token, Position, Literal) {
-        if !self.ch.is_ascii_digit() && is_letter(self.ch) {
-            let pos = self.position();
+    fn skip_whitespace(&mut self) {
+        while self.ch == b' ' || self.ch == b'\t' || self.ch == b'\n' || self.ch == b'\r' {
+            self.next();
+        }
+    }
 
+    fn expect(&mut self, c: u8) -> bool {
+        if self.peek() == c {
+            self.next();
+            true
+        } else {
+            false
+        }
+    }
+
+    fn switch(&mut self, def: Token, c: u8, t: Token) -> Token {
+        if self.peek() == c {
+            self.next();
+            t
+        } else {
+            def
+        }
+    }
+
+    fn switch2(&mut self, def: Token, c1: u8, t1: Token, c2: u8, t2: Token) -> Token {
+        match self.peek() {
+            c1 => {
+                self.next();
+                t1
+            }
+            c2 => {
+                self.next();
+                t2
+            }
+            _ => def,
+        }
+    }
+
+    fn switch3(
+        &mut self,
+        def: Token,
+        c1: u8,
+        t1: Token,
+        c2: u8,
+        t2: Token,
+        c3: u8,
+        t3: Token,
+    ) -> Token {
+        match self.peek() {
+            c1 => {
+                self.next();
+                t1
+            }
+            c2 => {
+                self.next();
+                t2
+            }
+            c3 => {
+                self.next();
+                t3
+            }
+            _ => def,
+        }
+    }
+
+    pub fn scan(&mut self) -> (Token, Position, Literal) {
+        self.skip_whitespace();
+        let pos = self.position();
+
+        if !self.ch.is_ascii_digit() && is_letter(self.ch) {
             let lit = self.scan_identifier();
             let tok = token::lookup(lit.as_str());
-
             return (tok, pos, lit);
         }
 
-        todo!()
+        if is_digit(self.ch) {
+            let (tok, lit) = self.scan_number();
+            return (tok, pos, lit);
+        }
+
+        let tok = match self.ch {
+            b'=' => self.switch(Token::ASSIGN, b'=', Token::EQL),
+            b'!' => self.switch(Token::NOT, b'=', Token::NEQ),
+            b'+' => self.switch2(Token::PLUS, b'+', Token::INC, b'=', Token::PLUS_ASSIGN),
+            b'-' => self.switch3(
+                Token::MINUS,
+                b'+',
+                Token::DEC,
+                b'=',
+                Token::MINUS_ASSIGN,
+                b'>',
+                Token::ARROW,
+            ),
+
+            b'(' => Token::LPAREN,
+            _ => Token::ILLEGAL,
+        };
+
+        self.next();
+
+        let lit = self.src[pos.offset..self.offset].to_string();
+
+        (tok, pos, lit)
     }
+}
+
+fn is_digit(c: u8) -> bool {
+    c >= b'0' && c <= b'9'
 }
 
 fn is_letter(c: u8) -> bool {
